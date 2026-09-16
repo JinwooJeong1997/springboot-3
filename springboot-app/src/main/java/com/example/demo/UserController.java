8ffa0f0277d268cd772183b7fcfda47932bae716
Jinwoo Jeong <jjwlove12@tukorea.ac.kr>
2026-09-16 11:03:58 +0900

    test1,2

--- springboot-app/src/main/java/com/example/demo/UserController.java
+++ springboot-app/src/main/java/com/example/demo/UserController.java
@@ -17,13 +17,27 @@ public class UserController {
 
     @GetMapping("/")
     public String hello_default() {
-        return " Spring Boot START PAGE CHANGED! MK4!!!!";
+        return "<body bgcolor='blue'>Spring Boot START PAGE CHANGED! MK4!!!!</body>";
     }
 
      @GetMapping("/mysql")
     public String dbTest() {
         try {
             String sql = "SELECT now()";
+            String result = jdbcTemplate.queryForObject(sql, 
+String.class);
+            return "Database test successful. now() : " + result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Database connection failed! Error: " + 
+e.getMessage();
+        }
+    }
+    # test mysql2
+    @GetMapping("/mysql2")
+    public String dbTest() {
+        try {
+            String sql = "SELECT id from member";
             String result = jdbcTemplate.queryForObject(sql, 
 String.class);
             return "Database test successful. now() : " + result;
