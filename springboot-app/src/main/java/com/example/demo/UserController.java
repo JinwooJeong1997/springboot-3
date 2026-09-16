15cb0000da04cea039149a4c559453ea4b01713f
Jinwoo Jeong <jjwlove12@tukorea.ac.kr>
2026-09-16 10:33:16 +0900

    aws test #4

--- springboot-app/src/main/java/com/example/demo/UserController.java
+++ springboot-app/src/main/java/com/example/demo/UserController.java
@@ -17,7 +17,7 @@ public class UserController {
 
     @GetMapping("/")
     public String hello_default() {
-        return " Spring Boot START PAGE CHANGED! MK3!!";
+        return " Spring Boot START PAGE CHANGED! MK3!!!";
     }
 
      @GetMapping("/mysql")
